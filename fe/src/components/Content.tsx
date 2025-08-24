b6864580142a4f92f087e405c256d6472928775a
crrashh1542 <1542@crrashh.com>
2025-08-24 19:31:54 +0800

    fix(fe): 修复主页代码块高亮失败的问题
    由于 highlight.js 在 Promise 动态更新 DOM 后需要手动调用，故将高亮方案换为可自动在 React
    环境下更新状态的 react-syntax-highlighter，无需手动触发
    
    Signed-off-by: crrashh1542 <1542@crrashh.com>

--- fe/src/components/Content.tsx
+++ fe/src/components/Content.tsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from 'react'
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
+import javascript from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript'
+import { atomOneLight } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 import Catalog from './widgets/Catalog'
-import CodeBlock from "./widgets/CodeBlock"
 
 import sitePromise from '../utils/getSite'
 import '../styles/content.less'
+
+// Light 引入的高亮语法需要手动注册
+SyntaxHighlighter.registerLanguage('javascript', javascript)
 
 export default () => {
     // 将 site 的 Promise 对象解析并保存到 site 变量里
@@ -21,16 +26,16 @@ export default () => {
         <main>
             <Catalog>默认配置</Catalog>
             <p>将你原来引用 CDNJS 静态资源的地址：</p>
-            <CodeBlock code={'<script src="https://cdnjs.cloudflare.com/ajax/libs/react/19.1.1/cjs/react.production.min.js"></script>'} />
+            <SyntaxHighlighter language="xml" style={ atomOneLight } className="code-block">{'<script src="https://cdnjs.cloudflare.com/ajax/libs/react/19.1.1/cjs/react.production.min.js"></script>'}</SyntaxHighlighter>
             <p>改为本站点域名即可！</p>
-            <CodeBlock code={'<script src="' + site + '/react/19.1.1/cjs/react.production.min.js"></script>'} />
+            <SyntaxHighlighter language="xml" style={ atomOneLight } className="code-block">{'<script src="' + site + '/react/19.1.1/cjs/react.production.min.js"></script>'}</SyntaxHighlighter>
             <br /><br />
 
             <Catalog>校验内容</Catalog>
             <p>如果担心被劫持，可以在标签中加入 <code>{'integrity'}</code> 属性用于验证。</p>
-            <CodeBlock code={'integrity="sha512-{对应的 SHA512 哈希}"'} />
+            <SyntaxHighlighter language="xml" style={ atomOneLight } className="code-block">{'integrity="sha512-{对应的 SHA512 哈希}"'}</SyntaxHighlighter>
             <p>举个栗子，当引入 CSS 文件时，食用方法如下：</p>
-            <CodeBlock code={'<link rel="stylesheet" href="' + site + '/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="/>'} />
+            <SyntaxHighlighter language="xml" style={ atomOneLight } className="code-block">{'<link rel="stylesheet" href="' + site + '/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="/>'}</SyntaxHighlighter>
 
         </main>
     )
